1f3f1df214660e502b83ffc535d87e701a71bc4a
cauematheus23 <cauematheus_23@hotmail.com>
2024-06-04 17:20:42 -0300

    Inserir no banco pronto

--- IngestaoDados/5by5-DapperGaragemCarros/Program.cs
+++ IngestaoDados/5by5-DapperGaragemCarros/Program.cs
@@ -4,11 +4,6 @@ using Models;
 
 string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Documents\\Dados\\carros.json";
 List<Carro> teste = ReadFile.GetCarros(path);
-
-foreach (var item in teste)
-{
-    Console.WriteLine(item);
-}
 CarroController carroController = new CarroController();
 
 if (carroController.Insert(teste))
